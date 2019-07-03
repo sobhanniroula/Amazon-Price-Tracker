7c7e7b1c3b468b220e6e1f35e17bb948926da47c
sobhanniroula <sobhan_niroula@yahoo.com>
2019-07-03 22:45:54 +0300

    Initial commit: basic files added

---
+++ tracker.py
@@ -0,0 +1,6 @@
+import requests
+
+URL = 'https://www.amazon.de/Canon-Foto%C4%9Fraf-Makinesi-Megapiksel-Ekran-Schwarz/dp/B01L8U5924/ref=sr_1_1?__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2QIBX069RVRU8&keywords=canon+5d+mark+iv&qid=1562182791&s=gateway&sprefix=canon+5d%2Caps%2C163&sr=8-1';
+
+headers = {"User-Agent" : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'}
+
